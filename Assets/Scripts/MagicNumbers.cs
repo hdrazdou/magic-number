aabe53e353c16afc2eb13a9bec207f8004441878
Hleb Drazdou <Hleb.Drazdou@gmail.com>
2023-06-24 21:58:04 +0200

    added main logic

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicNumbers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Min = 1;
+    public int Max = 3000;
+    private int _guess;
+    
+    private void Start()
     {
-        
+        Debug.Log($"Make a number from {Min} to {Max}.");
+        CalcualateGuess();
+        AskAboutGuess();
+    }
+
+    private void AskAboutGuess()
+    {
+        Debug.Log($"Your number is {_guess}?");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Max = _guess;
+            CalcualateGuess();
+            AskAboutGuess();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Min = _guess;
+            CalcualateGuess();
+            AskAboutGuess();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log($"Congrats! I've guessed, your number is {_guess}.");
+        }
+    }
+
+    private void CalcualateGuess()
+    {
+        _guess = (Min + Max) / 2;
     }
 }

f5dccfa779faaa8e576d699acd9b448d77cc884a
Hleb Drazdou <Hleb.Drazdou@gmail.com>
2023-06-25 21:41:09 +0200

    added showing count of moves and displaying debug.log to screen, not console

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -5,20 +5,22 @@ public class MagicNumbers : MonoBehaviour
     public int Min = 1;
     public int Max = 2000;
     private int _guess;
+    private int _moves;
     
     private void Start()
     {
         Debug.Log($"Make a number from {Min} to {Max}.");
         CalcualateGuess();
         AskAboutGuess();
+        
     }
 
     private void AskAboutGuess()
     {
         Debug.Log($"Your number is {_guess}?");
+        _moves++;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -37,7 +39,13 @@ public class MagicNumbers : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log($"Congrats! I've guessed, your number is {_guess}.");
+            Debug.Log($"Congrats! I've guessed, your number is {_guess}. It took {_moves} moves to guess.");
+            Debug.Log("------------------------------------------------");
+            Min = 1;
+            Max = 2000;
+            _moves = 0;
+            _guess = 0;
+            Start();
         }
     }
 
